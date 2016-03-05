114e87a53d2506a7176ed9ab6d6faf84b07fcb3e
Murray Cumming <murrayc@murrayc.com>
2016-03-05 09:39:49 +0100

    retype.h: Remove comment saying it is generated.

--- sigc++/adaptors/retype.h
+++ sigc++/adaptors/retype.h
@@ -1,7 +1,6 @@
-// -*- c++ -*-
-/* Do not edit! -- generated file */
 #ifndef _SIGC_ADAPTORS_RETYPE_H_
 #define _SIGC_ADAPTORS_RETYPE_H_
+
 #include <sigc++/adaptors/adaptor_trait.h>
 #include <sigc++/functors/ptr_fun.h>
 #include <sigc++/functors/mem_fun.h>
